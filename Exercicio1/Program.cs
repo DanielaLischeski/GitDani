be237301ab5dd8ab2ff50604cf02a466202e2145
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 14:14:20 -0300

    Resolução exercício 1.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<><><><><><><><><><><><><><><><><><><><><><><><><>");
+            Console.WriteLine("<><>                                          <><>");
+            Console.WriteLine("<><>             Você quer beber????          <><>");
+            Console.WriteLine("<><>                                          <><>");
+            Console.WriteLine("<><><><><><><><><><><><><><><><><><><><><><><><><>\r\n");
+
+            Console.WriteLine("Então digite seu nome:");
+            string nome = Console.ReadLine();
+
+            //int.TryParse(Console.ReadLine(),)
+
+            Console.WriteLine("\r\nE agora digite sua idade:");
+
+            int.TryParse(Console.ReadLine(), out int idade);
+
+            if(idade >= 18)
+            {
+                Console.WriteLine($"\r\nOba! {nome} você possui {idade} anos e é maior de idade, pode consumir bedida alcoólica! Mas se beber não dirija!");
+            }
+            else
+            {
+                Console.WriteLine($"\r\nQue pena {nome}! Você possui {idade} anos e é menor de idade. Beba água!");
+            }
+
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ SistemaIdade/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string[] stringInfo = new string[2];
+
+           // for (int i = 0; i < stringInfo.Length; i++)
+           // {
+                stringInfo[i] = $""
+           // }
+
+           // if ( >= 18)
+            //{
+
+           // }
+
+
+            //Console.WriteLine("Digite sua idade:");
+
+          
+        }
+
+        public static void BaseDeDados()
+        {
+
+        }
+                     
+    }
+}
