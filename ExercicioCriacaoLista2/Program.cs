b18a8977605e81eb20ee8eb13caa86a618bd6605
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 13:32:15 -0300

    Exercício criação de listas, 4 colunas.

--- ExercicioCriacaoLista2/Program.cs
+++ ExercicioCriacaoLista2/Program.cs
@@ -10,69 +10,74 @@ namespace ExercicioCriacaoLista2
     {
         static void Main(string[] args)
         {
-            string[,] listaDeNome = new string[2, 3];
+            string[,] listaDeCarro = new string[2, 4];
 
             int IdParaLista = 0;
 
-            InsereRegistro(ref listaDeNome, ref IdParaLista);
+            InsereRegistro(ref listaDeCarro, ref IdParaLista);
 
             Console.ReadKey();
 
-            InsereRegistro(ref listaDeNome, ref IdParaLista);
+            InsereRegistro(ref listaDeCarro, ref IdParaLista);
 
             Console.ReadKey();
         }
         /// <summary>
         /// Metodo que insere novo registro dentro da nossa lista 
         /// </summary>
-        /// <param name="listaDeNome">Nossa lista de nomes vazia ou não</param>
+        /// <param name="listaDeCarro">Nossa lista de nomes vazia ou não</param>
         /// <param name="IdParaLista">Nossa referencia externa de id</param>
-        public static void InsereRegistro(ref string[,] listaDeNome, ref int IdParaLista)
+        public static void InsereRegistro(ref string[,] listaDeCarro, ref int IdParaLista)
         {
 
-            for (int i = 0; i < listaDeNome.GetLength(0); i++)
+            for (int i = 0; i < listaDeCarro.GetLength(0); i++)
             {
-                if (listaDeNome[i, 0] != null)
+                if (listaDeCarro[i, 0] != null)
                     continue;
-                Console.WriteLine("\r\nInforma um nome para adicionar um registro:");
-                var nome = Console.ReadLine();
+                Console.WriteLine("\r\nInforma o modelo de carro para adicionar um registro:");
+                var modelo = Console.ReadLine();
 
-                Console.WriteLine("\r\nInforma uma idade para adicionar um registro:");
-                var idade = Console.ReadLine();
+                Console.WriteLine("\r\nInforma o ano para adicionar um registro:");
+                var ano = Console.ReadLine();
 
-                listaDeNome[i, 0] = (IdParaLista++).ToString();
+                Console.WriteLine("\r\nInforma a cor do carro para adicionar um registro:");
+                var cor = Console.ReadLine();
 
-                listaDeNome[i, 1] = nome;
+                listaDeCarro[i, 0] = (IdParaLista++).ToString();
 
-                listaDeNome[i, 2] = idade;
+                listaDeCarro[i, 1] = modelo;
 
-                Console.WriteLine("Deseja inserir um novo registro? sim(1) ou não(0)");
+                listaDeCarro[i, 2] = ano;
+
+                listaDeCarro[i, 3] = cor;
+
+                Console.WriteLine("\r\nDeseja inserir um novo registro? sim(1) ou não(0)");
 
                 var continuar = Console.ReadKey().KeyChar.ToString();
 
                 if (continuar == "0")
                     break;
 
-                AumentaTamanhoLista(ref listaDeNome);
+                AumentaTamanhoLista(ref listaDeCarro);
             }
 
             Console.WriteLine("Registro adicionados com sucesso, segue lista de informações adicionadas:");
 
 
-            for (int i = 0; i < listaDeNome.GetLength(0); i++)
+            for (int i = 0; i < listaDeCarro.GetLength(0); i++)
 
-                Console.WriteLine(string.Format("Registro ID {0} - Nome:{1} - Idade: {2}", listaDeNome[i, 0], listaDeNome[i, 1], listaDeNome[i, 2]));
+                Console.WriteLine(string.Format("Registro ID {0} - Nome: {1} - Ano: {2} - Cor: {3}", listaDeCarro[i, 0], listaDeCarro[i, 1], listaDeCarro[i, 2], listaDeCarro[i, 3]));
         }
 
-        public static void AumentaTamanhoLista(ref string[,] ListaDeNome)
+        public static void AumentaTamanhoLista(ref string[,] ListaDeCarro)
         {
 
             var limiteDaLista = true;
 
-            for (int i = 0; i < ListaDeNome.GetLength(0); i++)
+            for (int i = 0; i < ListaDeCarro.GetLength(0); i++)
             {
 
-                if (ListaDeNome[i, 0] == null)
+                if (ListaDeCarro[i, 0] == null)
                     limiteDaLista = false;
             }
 
@@ -80,20 +85,22 @@ namespace ExercicioCriacaoLista2
             if (limiteDaLista)
             {
 
-                var listaCopia = ListaDeNome;
+                var listaCopia = ListaDeCarro;
 
-                ListaDeNome = new string[ListaDeNome.GetLength(0) + 5, 3];
+                ListaDeCarro = new string[ListaDeCarro.GetLength(0) + 2, 4];
 
                 for (int i = 0; i < listaCopia.GetLength(0); i++)
                 {
-                    ListaDeNome[i, 0] = listaCopia[i, 0];
+                    ListaDeCarro[i, 0] = listaCopia[i, 0];
 
-                    ListaDeNome[i, 1] = listaCopia[i, 1];
+                    ListaDeCarro[i, 1] = listaCopia[i, 1];
 
-                    ListaDeNome[i, 2] = listaCopia[i, 2];
+                    ListaDeCarro[i, 2] = listaCopia[i, 2];
+
+                    ListaDeCarro[i, 3] = listaCopia[i, 3];
                 }
 
-                Console.WriteLine("O tamanho da lista foi atualizado.");
+                Console.WriteLine("\r\nO tamanho da lista foi atualizado.");
             }
 
         }
