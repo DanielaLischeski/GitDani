c3e3a185d7a7bbde307ab493187c57cc4406617b
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 08:55:04 -0300

    Limpando os valores inseridos

--- CriacaoDeLista/Program.cs
+++ CriacaoDeLista/Program.cs
@@ -16,13 +16,24 @@ namespace CriacaoDeLista
 
             //laço que usamos geralmente para andar sobre as posições da noss alista
             for (int i = 0; i < lista.Length; i++)
-            {
+            
                 //caregando os valores da nossa lsita aqui
                 //onde "i" representa cada espaço que temos disponível
                 lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
                 //DateTime é um tipo do .net c# que diponibiliza funções relacionadas a datas
                 //e horas, ou seja, se precisar trabalhar com essas informações é possíveç ussa este cara.
-            }
+            
+
+            //Laço de repetição que usamos para varrer nossa lista de maneira mais simples
+            foreach (var item in lista) //"var item" idica uma unidade da nossa lista "in lista"
+                //indica a lista que desejamos varrer
+                Console.WriteLine(item); //Aqui apresentamos essa informação na tela.
+
+            Console.ReadKey();
+
+            for (int i = 0; i < lista.Length; i++)
+            
+                lista[i] = string.Empty; //Aqui limpamos o nosso valor dentro da coleção
 
             //Laço de repetição que usamos para varrer nossa lista de maneira mais simples
             foreach (var item in lista) //"var item" idica uma unidade da nossa lista "in lista"
