f5fbf62e31ed7630fbfb4edabeea9078bcf3d294
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 13:18:03 -0300

    Exercício criação de listas.

---
+++ ExercicioCriacaoLista/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCriacaoLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    
+            //Criançao da lista o.0.
+            string[,] listaDeNome = new string[2, 3];
+            //Aqui vamos criar uma forma externa de identificar nossos registros
+            int IdParaLista = 0;
+
+            //chamada do nosso método para inserir registro passando por meio de referência
+            //nossos dois itens, lista de nomes e nosso identificador único.
+            InsereRegistro(ref listaDeNome, ref IdParaLista);
+
+            Console.ReadKey();
+
+            //Aqui adicionamos novamente nosso insere registros
+            InsereRegistro(ref listaDeNome, ref IdParaLista);
+
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// Método que insere novo registro dentro da nossa lista
+        /// </summary>
+        /// <param name="listaDeNome">Nossa lista de nomes vazia ou não</param>
+        /// <param name="IdParaLista">Npossa referência externa de id</param>
+        public static void InsereRegistro(ref string[,] listaDeNome, ref int IdParaLista)
+        {
+            //Aqui estaremos em um laço para informar nossos registros
+            for (int i = 0; i < listaDeNome.GetLength(0); i++)
+            {
+                //Aqui definimos que o mesmo deve continuar para o próximo registro
+                //pois esse registro já está ocupado
+                if (listaDeNome[i, 0] != null) //Dani: sem este if  e continue ele sobrescreve as informações
+                    continue;
+                //indicamos que ele deve apenas informar o nome do nosso registro
+                Console.WriteLine("\r\nInforma um nome para adicionar um registro:");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("\r\nInforma uma idade para adicionar um registro:");
+                var idade = Console.ReadLine();
+                //Criamos o nosso identificar único com um objeto externo que
+                //mesmo após sairmos do nosso laço ainda poderá ser incrementado
+                listaDeNome[i, 0] = (IdParaLista++).ToString();
+                //Aqui colocamos nosso nome na lista
+                listaDeNome[i, 1] = nome;
+
+                listaDeNome[i, 2] = idade;
+
+                //Identificamos se o mesmo ainda deseja inserir registros dentro da nossa lista
+                Console.WriteLine("Deseja inserir um novo registro? sim(1) ou não (0)");
+                //O readKey só espera uma única tecla e nos retorna a tecla que foi acionada e não o valor dela
+                //Por isso usamos KeyChar para pegar esse valor e converter ele em um string para comparação
+                var continuar = Console.ReadKey().KeyChar.ToString();
+                //Aqui validamos a condição se o mesmo deve ou não continuar a adicionar registro até que a nossa
+                //lista esteja completa de informações
+                if (continuar == "0")
+                    break; //Break é uma palavra reservada do c# que para, por isso break, todo laço de repetição
+                           //ou sequenciador lógico o.0.
+            }
+
+            Console.WriteLine("Registro adicionado com sucsso, segue lista de informações adicionadas:");
+
+            //Mas agora a coisa muda,
+            for (int i = 0; i < listaDeNome.GetLength(0); i++)
+                //Utilizamos o string format, basicamente ele faz a mesma forma que o sifrão
+                //a diferença entre eles é que este é um cara em grandes quantidades
+                //acaba sendo mais rápido
+                Console.WriteLine(string.Format("Registro ID {0} - Nome: {1} - Idade: {2}", listaDeNome[i, 0], listaDeNome[i, 1], listaDeNome[i,2]));
+
+
+        }
+    }
+}
---
+++ ExercicioCriacaoLista2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCriacaoLista2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeNome = new string[2, 3];
+
+            int IdParaLista = 0;
+
+            InsereRegistro(ref listaDeNome, ref IdParaLista);
+
+            Console.ReadKey();
+
+            InsereRegistro(ref listaDeNome, ref IdParaLista);
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Metodo que insere novo registro dentro da nossa lista 
+        /// </summary>
+        /// <param name="listaDeNome">Nossa lista de nomes vazia ou não</param>
+        /// <param name="IdParaLista">Nossa referencia externa de id</param>
+        public static void InsereRegistro(ref string[,] listaDeNome, ref int IdParaLista)
+        {
+
+            for (int i = 0; i < listaDeNome.GetLength(0); i++)
+            {
+                if (listaDeNome[i, 0] != null)
+                    continue;
+                Console.WriteLine("\r\nInforma um nome para adicionar um registro:");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("\r\nInforma uma idade para adicionar um registro:");
+                var idade = Console.ReadLine();
+
+                listaDeNome[i, 0] = (IdParaLista++).ToString();
+
+                listaDeNome[i, 1] = nome;
+
+                listaDeNome[i, 2] = idade;
+
+                Console.WriteLine("Deseja inserir um novo registro? sim(1) ou não(0)");
+
+                var continuar = Console.ReadKey().KeyChar.ToString();
+
+                if (continuar == "0")
+                    break;
+
+                AumentaTamanhoLista(ref listaDeNome);
+            }
+
+            Console.WriteLine("Registro adicionados com sucesso, segue lista de informações adicionadas:");
+
+
+            for (int i = 0; i < listaDeNome.GetLength(0); i++)
+
+                Console.WriteLine(string.Format("Registro ID {0} - Nome:{1} - Idade: {2}", listaDeNome[i, 0], listaDeNome[i, 1], listaDeNome[i, 2]));
+        }
+
+        public static void AumentaTamanhoLista(ref string[,] ListaDeNome)
+        {
+
+            var limiteDaLista = true;
+
+            for (int i = 0; i < ListaDeNome.GetLength(0); i++)
+            {
+
+                if (ListaDeNome[i, 0] == null)
+                    limiteDaLista = false;
+            }
+
+
+            if (limiteDaLista)
+            {
+
+                var listaCopia = ListaDeNome;
+
+                ListaDeNome = new string[ListaDeNome.GetLength(0) + 5, 3];
+
+                for (int i = 0; i < listaCopia.GetLength(0); i++)
+                {
+                    ListaDeNome[i, 0] = listaCopia[i, 0];
+
+                    ListaDeNome[i, 1] = listaCopia[i, 1];
+
+                    ListaDeNome[i, 2] = listaCopia[i, 2];
+                }
+
+                Console.WriteLine("O tamanho da lista foi atualizado.");
+            }
+
+        }
+    }
+}
