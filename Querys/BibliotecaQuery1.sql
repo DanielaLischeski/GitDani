739a00bcc56192f81d3c24e9c4fc91df9b79b406
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 14:17:24 -0300

    Tableas relacionadas - biblioteca

--- Querys/BibliotecaQuery1.sql
+++ Querys/BibliotecaQuery1.sql
@@ -1,7 +1,8 @@
-﻿
-select 	
+﻿select 	
 		BL.Nome,
-		COUNT(LV.Id) as 'QuantidadeDeLivros'
+		COUNT(LV.Id) as 'QuantidadeDeLivros',
+		US.Nome as 'UserName'
 	from Biblioteca BL
 	inner join Livros LV on BL.Id = LV.Biblioteca
-	group by BL.Nome
+	inner join Usuario US on LV.UsuInc = US.Id
+	group by BL.Nome,US.Nome
