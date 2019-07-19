c28ef73bf103e9dfb1d21805f6c42ce380b1807f
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 16:25:49 -0300

    Exercício 1 de banco de dados

--- ExerciciosBancoDeDados/Query Media.sql
+++ ExerciciosBancoDeDados/Query Media.sql
@@ -1,6 +1,7 @@
 ﻿select 
 	AL.Nome,
-	SUM(NT.Nota) / COUNT(*) as 'Media'
+	SUM(NT.Nota) / COUNT(*) as 'Media',
+	IIF(SUM(NT.Nota) / COUNT(*) >= 7,'Nota suficiente','Nota insuficiente') as 'Status Nota'
 from Alunos AL
 inner join Notas NT on AL.Id = NT.Aluno
 group by AL.Nome
