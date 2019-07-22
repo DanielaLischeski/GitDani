94c5ec7b9f6d9000a8958b5d8f9e2e79cea57e6f
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-22 12:59:20 -0300

    Review SQL

---
+++ Review/Query Select Nova Turma.sql
@@ -0,0 +1,11 @@
+﻿select 
+		a.Aluno,
+		SUM(n.Nota) / count(d.[Data]) as 'Média' 
+	from Alunos a
+		inner join TurmaAlunos tr on a.Id=tr.Aluno
+		inner join Diarios d on tr.Turma=d.Turma
+		left join Notas n on d.Id=n.Diario and a.Id=n.Aluno
+where tr.Turma = 2  --pega os alunos da turma 1
+group by a.Aluno
+
+
