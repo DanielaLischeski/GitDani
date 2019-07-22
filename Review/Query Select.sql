e1798d12bf117936e53825b547af353d2d6b3e6f
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-22 11:13:23 -0300

    Review SQL

--- Review/Query Select.sql
+++ Review/Query Select.sql
@@ -56,10 +56,22 @@ select
 	group by d.[Data],tr.Aluno,n.Nota
 
 
-	select 
+select 
 		d.[Data],  
 		tr.Aluno,
 		n.Nota as 'Média',
+		IIF(ISNULL(p.Id,0) >  0,'Presente','Ausente') as 'Presenças'
+	from Diarios d 
+		inner join TurmaAlunos tr on d.Turma=tr.Turma		
+		left join Presenca p on d.Id=p.Diario and tr.Aluno=p.Aluno
+		left join Notas n on tr.Aluno=n.Aluno and d.Id=n.Diario
+
+
+select 
+		d.[Data],  
+		tr.Aluno,
+		--n.Nota as 'Média',
+		ISNULL(n.Nota,0) as 'Nota',
 		IIF(ISNULL(p.Id,0) >  0,'Presente','Ausente') as 'Presenças'
 	from Diarios d 
 		inner join TurmaAlunos tr on d.Turma=tr.Turma		
