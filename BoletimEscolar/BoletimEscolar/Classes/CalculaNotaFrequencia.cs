47fa45f4e255b94a310e5a2a2574e4f40158de05
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-25 09:28:28 -0300

    Boletim Escolar

---
+++ BoletimEscolar/BoletimEscolar/Classes/CalculaNotaFrequencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletimEscolar.Clases
+{
+    public class CalculaMedia
+    {
+        
+        public int CalculoDeMedia(int nota1, int nota2, int nota3)
+        {
+            return (nota1 + nota2 + nota3) / 3;         
+        }
+
+        public int CalculoDeFrequencia(int totalAulas, int numeroFaltas)
+        {
+           return ((totalAulas - numeroFaltas) * 100)/totalAulas;   
+        }
+
+        public void SituacaoAluno(int media, int frequencia)
+        {
+            if ((media >= 7) && (frequencia >= 75))
+                Console.WriteLine($"Aluno apovado com média {media} e frequêcia {frequencia}%");
+            else
+                Console.WriteLine($"Aluno reprovado com média {media} e frequência {frequencia}%");
+        }
+    }
+}
---
+++ BoletimEscolar/VisualizaBoletimEscolar/VisualizaResultado.cs
@@ -0,0 +1,36 @@
+﻿using BoletimEscolar.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualizaBoletimEscolar
+{
+    class VisualizaResultado
+    {        
+        static void Main(string[] args)
+        {
+            CalculaMedia boletim = new CalculaMedia();
+
+            Console.WriteLine("Insira a primeira nota do alunos: ");
+            var n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira a segunda nota do alunos: ");
+            var n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira a terceira nota do alunos: ");
+            var n3 = Convert.ToInt32(Console.ReadLine());
+
+            var media = boletim.CalculoDeMedia(n1, n2, n3);
+                             
+            Console.WriteLine("Insira o número total de aulas: ");
+            var aulas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o número de faltas do aluno: ");
+            var faltas = Convert.ToInt32(Console.ReadLine());
+            var frequencia = boletim.CalculoDeFrequencia(aulas, faltas);
+
+            boletim.SituacaoAluno(media, frequencia);
+
+            Console.ReadKey();
+        }
+    }
+}
