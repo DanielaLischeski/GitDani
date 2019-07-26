fd0a4b9923ec99a74e95080a66110f18a3ecce13
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-26 15:22:22 -0300

    Exercício calculadora DLL

---
+++ AcessandoDLLMarlon/AcessandoDLLMarlon/Program.cs
@@ -0,0 +1,34 @@
+﻿using Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessandoDLLMarlon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usandoCaluladoraMarlon = new Operador();
+
+            Console.WriteLine($"Soma: {usandoCaluladoraMarlon.Adicao(20.2,22)}");
+
+            Console.WriteLine($"Divisao: {usandoCaluladoraMarlon.Divisao(50,25)}");
+
+            Console.WriteLine($"Multiplicacao: {usandoCaluladoraMarlon.Multiplicacao(1.5, 4)}");
+
+            Console.WriteLine($"Raio do circulo: {usandoCaluladoraMarlon.RaioDoCirculo(65)}");
+
+            Console.WriteLine($"Retangulo: {usandoCaluladoraMarlon.Retangulo(2.5,8)}");
+
+            Console.WriteLine($"Subtração: {usandoCaluladoraMarlon.Subtracao(26,32)}");
+
+            Console.WriteLine($"Triangulo: {usandoCaluladoraMarlon.TrianguloEquilatero(7)}");
+
+            Console.ReadKey();
+         
+        }
+    }
+}
--- CalculadoraClass/UsandoCalculadora/Program.cs
+++ CalculadoraClass/UsandoCalculadora/Program.cs
@@ -12,6 +12,8 @@ namespace UsandoCalculadora
         static void Main(string[] args)
         {
             FuncoesCalculadora bbCalc = new FuncoesCalculadora();
+            //é o mesmo que:
+            //var bbCalc = new FuncoesCalculadora();
 
             Console.WriteLine($"Soma = {bbCalc.CalculaAdicao(5, 5)}");
 
@@ -20,7 +22,13 @@ namespace UsandoCalculadora
             Console.WriteLine($"Multiplicação = {bbCalc.CalculaMultiplicacao(2, 4)}");
 
             Console.WriteLine($"Divisao = {bbCalc.CalculaDivisao(10, 5)}");
-            
+
+            Console.WriteLine($"Calcula área de retângulo = {bbCalc.CalculaAreaRetangulo(2,4)}");
+
+            Console.WriteLine($"Calcula área de triângulo equilátero = {bbCalc.CalculaAreaTrianguloEquilatero(5)}");
+
+            Console.WriteLine($"Calcula raio de círculo = {bbCalc.CalculaRaioCirculo(12)}");
+
             Console.ReadKey();
         }
     }
