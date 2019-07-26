092d6ed11f4df2f3d12086821806472b269b0d10
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-26 11:21:13 -0300

    Evolução de calculadora

---
+++ CalculadoraClass/UsandoCalculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using CalculadoraClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncoesCalculadora bbCalc = new FuncoesCalculadora();
+
+            Console.WriteLine($"Soma = {bbCalc.CalculaAdicao(5, 5)}");
+
+            Console.WriteLine($"Subtração = {bbCalc.CalculaSubtracao(12, 5)}");
+
+            Console.WriteLine($"Multiplicação = {bbCalc.CalculaMultiplicacao(2, 4)}");
+
+            Console.WriteLine($"Divisao = {bbCalc.CalculaDivisao(10, 5)}");
+            
+            Console.ReadKey();
+        }
+    }
+}
