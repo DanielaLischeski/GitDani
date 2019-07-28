3f298222b45705c424e93997a227a8536b6eb99e
DanielaLischeski <danielalischeski@gmail.com>
2019-07-28 13:42:43 -0300

    Exercício para casa 28/07/2019 - versão get set

---
+++ ProjetoAnimalElaborado/ProjetoAnimalElaborado/Classes/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAnimalElaborado.Classes
+{
+    public class Animal
+    {        
+        public string Nome { get; set; }
+        
+        public string Acordar()
+        {
+            return "acordou";
+        }
+
+        public string Comer()
+        {
+            return "está comendo";
+        }
+
+        public string Dormir()
+        {
+            return "está dormindo";
+        }
+
+
+    }
+}
---
+++ ProjetoAnimalElaborado/ProjetoAnimalElaborado/Program.cs
@@ -0,0 +1,49 @@
+﻿using ProjetoAnimalElaborado.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAnimalElaborado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal monitoraAnimal = new Animal();
+
+            monitoraAnimal.Nome = "Elefante";
+
+            Console.WriteLine($"{monitoraAnimal.Nome}");
+
+            Console.WriteLine($"{monitoraAnimal.Nome} {monitoraAnimal.Acordar()}");
+
+            Console.WriteLine($"{monitoraAnimal.Nome} {monitoraAnimal.Comer()}");
+
+            Console.WriteLine($"{monitoraAnimal.Nome} {monitoraAnimal.Dormir()}");
+
+            Console.ReadKey();
+
+            Console.Clear();
+
+            Rotina monitoraRotina = new Rotina();
+
+            monitoraRotina.Nome = "Lebre";
+
+            monitoraRotina.Idade = 2;
+
+            Console.WriteLine($"{monitoraRotina.Nome}");
+            
+            Console.WriteLine($"{monitoraRotina.Nome} possui {monitoraRotina.Idade} anos");
+
+            Console.WriteLine($"{monitoraRotina.Nome} {monitoraRotina.Acordar()}");
+
+            Console.WriteLine($"{monitoraRotina.Nome} {monitoraRotina.Comer()}");
+
+            Console.WriteLine($"{monitoraRotina.Nome} {monitoraRotina.Dormir()}");
+
+            Console.ReadKey();
+        }
+    }
+}
