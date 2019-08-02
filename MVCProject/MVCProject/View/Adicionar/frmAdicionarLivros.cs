6766952aa41f31634d4628106fe31a6175f47097
Treinamento HBSIS <proway@hbsis.com.br>
2019-08-02 17:20:41 -0300

    Exercício para casa 02/08/2019

---
+++ MVCProject/MVCProject/View/Adicionar/frmAdicionarLivros.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVCProject.View.Adicionar
+{
+    public partial class frmAdicionarLivros : Form
+    {
+        public frmAdicionarLivros()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAdicionarLivros_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sistemaBibliotecaDBDataSet.Editoras' table. You can move, or remove it, as needed.
+            this.editorasTableAdapter.Fill(this.sistemaBibliotecaDBDataSet.Editoras);
+            // TODO: This line of code loads data into the 'sistemaBibliotecaDBDataSet.Generos' table. You can move, or remove it, as needed.
+            this.generosTableAdapter.Fill(this.sistemaBibliotecaDBDataSet.Generos);
+
+        }
+    }
+}
---
+++ MVCProject/MVCProject/View/frmPrincipal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVCProject.View
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void UsuáriosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AutorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LivrosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LocaçãoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditorasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
