53d8ab4f6ab0e5f45696b751e4fadd503a9d5244
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-09 11:27:46 -0300

    Manhã 09/08/2019

---
+++ WPFAppEntity/WPFAppEntity/Views/ucControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFAppEntity.Data;
+
+namespace WPFAppEntity.Views
+{
+    /// <summary>
+    /// Interaction logic for usControl.xaml
+    /// </summary>
+    public partial class ucControl : UserControl
+    {
+        public ucControl()  //deveria se chamar ucLogin
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler sucess;
+        public event EventHandler fail;
+
+        public BibliotecaDBContext context = new BibliotecaDBContext();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var userName = tbxLogin.Text;
+            var userPass = tbxSenha.Password;
+
+            var result = context.Usuarios.FirstOrDefault(x => x.Login == userName && x.Senha == userPass);
+
+            if (result?.Id > 0)
+                sucess("Usuário logado com sucesso!", new EventArgs());
+            else
+                fail($"Falha ao logar com usuário {userName}.", new EventArgs());
+        }
+    }
+}
